7a26d35bf69e5d01b8ec8d34ccc9158f8dc5c811
licjcb8 <38548801+licjcb8@users.noreply.github.com>
2018-05-14 12:05:26 +0900

    웨폰마스터1일

---
+++ WeaponMaster/Assets/scripts/DynmicAxis.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DynmicAxis : MonoBehaviour
+{
+    public StatusBar Hpbar;
+ 
+    public float m_fSpeed = 10.0F;
+    public float m_fRotSpeed = 100.0F;
+    public float dmg = 20;
+    public float hpmax = 100;
+    public float hp =100;
+    
+
+
+    void Update()
+    {
+        //float translation = Input.GetAxis("Vertical") * speed;
+        //float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+        //translation *= Time.deltaTime;
+        //rotation *= Time.deltaTime;
+        //transform.Translate(0, 0, translation);
+        //transform.Rotate(0, rotation, 0);
+    }
+
+    private void FixedUpdate()
+    {
+        float fVertical = Input.GetAxis("Vertical");
+        float fHorizontal = Input.GetAxis("Horizontal");
+        GetComponent<Rigidbody>().AddForce(transform.forward * m_fSpeed);
+
+        if (Mathf.Abs(fVertical) > 0 || Mathf.Abs(fHorizontal) > 0)
+        {
+            float fTranslation = fVertical * m_fSpeed;
+            float fRotation = fHorizontal * m_fRotSpeed;
+            fTranslation *= Time.deltaTime;
+            fRotation *= Time.deltaTime;
+            transform.Translate(0, 0, fTranslation);
+            transform.Rotate(0, fRotation, 0);
+        }
+
+        GUI.Box(new Rect(0, 0, 300, 100),
+                "Vertical:" + fVertical +
+                "\nHorizontal:" + fHorizontal);
+    }
+     void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Monster")
+        {
+            hp = hp - dmg;
+            if (hp == 0)
+            { Destroy(collision.gameObject); }
+        }
+    }
+
+}
---
+++ WeaponMaster/Assets/scripts/StatusBar.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StatusBar : MonoBehaviour {
+    public RectTransform m_cRectTransform;
+    float m_fMax;
+	// Use this for initialization
+	void Start () {
+        m_cRectTransform = this.gameObject.GetComponent<RectTransform>();
+        m_fMax = m_cRectTransform.sizeDelta.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnGUI()
+    {
+        if(GUI.Button(new Rect(0,0,100,10),""))
+        {
+            Set(10,100);
+        }
+    }
+
+    public void Set(float cur, float max)
+    {
+        float fVal = cur / max;
+
+        float fStatusBar = m_fMax * fVal;
+
+        m_cRectTransform.sizeDelta = new Vector3(fStatusBar, m_cRectTransform.sizeDelta.y);
+    }
+}
