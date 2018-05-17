76a8733b31e99c2abc75f88af0ee954317c15aeb
licjcb8 <38548801+licjcb8@users.noreply.github.com>
2018-05-17 12:50:33 +0900

    4일

---
+++ WeaponMaster/Assets/scripts/Navigate.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class Navigate : MonoBehaviour {
+    public Transform m_Target;
+    public float m_fMinDist = 5;
+    NavMeshAgent m_cNavMeshAgent;
+    float m_fDist = 0;
+	// Use this for initialization
+	void Start () {
+        m_cNavMeshAgent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        TracingTarget();
+	}
+
+    void TracingTarget()
+    {
+        Vector3 vTargetPos = m_Target.position;
+        Vector3 vPos = transform.position;
+
+        m_fDist = Vector3.Distance(vTargetPos, vPos);
+
+        if (m_fDist > m_fMinDist)
+            m_cNavMeshAgent.SetDestination(vTargetPos);
+    }
+
+    private void OnGUI()
+    {
+        GUI.Box(new Rect(0, 0, 100, 20), "Dist:" + m_fDist);
+    }
+
+}
