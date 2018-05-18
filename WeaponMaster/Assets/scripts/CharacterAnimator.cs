733b65f3d97b74a3edf635178372734f646ef13d
licjcb8 <38548801+licjcb8@users.noreply.github.com>
2018-05-18 12:48:47 +0900

    d

---
+++ WeaponMaster/Assets/scripts/CharacterAnimator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimator : MonoBehaviour {
+    public enum eAnimate { NONE = -1, Wait, Walk, Attack }
+    public int cState = 0;
+    Animator animator;
+
+    public void SetState(int i )
+    {
+        cState = i;
+    }
+    // Use this for initialization
+    void Start () {
+        animator = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+
+        if (cState == 1)
+        {
+            animator.SetFloat("State", 1);
+        }
+	}
+}
--- WeaponMaster/Assets/scripts/DynmicAxis.cs
+++ WeaponMaster/Assets/scripts/DynmicAxis.cs
@@ -5,16 +5,17 @@ public class DynmicAxis : MonoBehaviour
 {
 
 
-   
-    Animator animator;
 
+
+    public Animator animator;
     public float m_fSpeed = 10.0F;
     public float m_fRotSpeed = 100.0F;
     public float m_fPower;
 
    void Start()
     {
-        animator = GetComponentInChildren<Animator>();
+        //animator = GetComponent<Animator>();
+
     }
 
     void Update()
@@ -23,14 +24,17 @@ public class DynmicAxis : MonoBehaviour
 
     }
 
+    int anistate=0;
+
+
     private void FixedUpdate()
     {
         float fVertical = Input.GetAxis("Vertical");
         float fHorizontal = Input.GetAxis("Horizontal");
 
         GetComponent<Rigidbody>().AddForce(transform.forward * m_fSpeed);
-       
-   
+
+
         if (Mathf.Abs(fVertical) > 0 || Mathf.Abs(fHorizontal) > 0)
         {
             float fTranslation = fVertical * m_fSpeed;
@@ -39,8 +43,22 @@ public class DynmicAxis : MonoBehaviour
             fRotation *= Time.deltaTime;
             transform.Translate(0, 0, fTranslation);
             transform.Rotate(0, fRotation, 0);
+
+            if (anistate != 1)
+            {
+                animator.SetInteger("State", 1);
+                anistate = 1;
+            }
+
         }
-     
+        else
+        {
+            if (anistate != 0)
+            {
+                animator.SetInteger("State", 0);
+                anistate = 0;
+            }
+        }
     }
 
 
