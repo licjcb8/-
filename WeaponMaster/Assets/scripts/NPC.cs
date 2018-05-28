9fd5e28dc71a39b96f1732bf2a2b83879f8849b4
licjcb8 <38548801+licjcb8@users.noreply.github.com>
2018-05-28 22:38:30 +0900

    1

---
+++ WeaponMaster/Assets/scripts/NPC.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour {
+
+    List<ItemManager.eItem> m_listInventory = new List<ItemManager.eItem>();
+
+    // Use this for initialization
+    public void Initialize()
+    {
+        m_listInventory.Add(ItemManager.eItem.ShortSword);
+        m_listInventory.Add(ItemManager.eItem.Shield);
+        m_listInventory.Add(ItemManager.eItem.Potion);
+    }
+
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
